2b7c4db86dac8b8b3b47d0b828e5ce0bcd3e9d84
ludik <bakhanetssimon@gmail.com>
2025-12-03 15:17:41 +0300

    Initial Commit

---
+++ TravelMaster/TravelMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelMasterApp.swift
+//  TravelMaster
+//
+//  Created by Simon Bakhanets on 03.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelMasterTests/TravelMasterTests.swift
@@ -0,0 +1,16 @@
+//
+//  TravelMasterTests.swift
+//  TravelMasterTests
+//
+//  Created by Simon Bakhanets on 03.12.2025.
+//
+
+import Testing
+
+struct TravelMasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
